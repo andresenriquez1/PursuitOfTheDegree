4f8a64bbe9ad2da167f326c8d5ba133a235d853d
Parky Sood <parkysood@Parkys-MacBook-Pro-2.local>
2024-05-31 19:03:48 -0700

    fixed control panel and birds eye view

--- main-scene.js
+++ main-scene.js
@@ -1,14 +1,4 @@
-import { defs, tiny } from './examples/common.js';
-import { Maze } from './maze.js';
-import { Player } from './player.js';
-
-const {
-    Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
-    Canvas_Widget, Code_Widget, Text_Widget
-} = tiny;
-
-//Extra imports from a previous assignment for experimentation sake
-/*
+import {defs, tiny} from './examples/common.js';
 import {Axes_Viewer, Axes_Viewer_Test_Scene} from "./examples/axes-viewer.js"
 import {Collision_Demo, Inertia_Demo} from "./examples/collisions-demo.js"
 import {Many_Lights_Demo} from "./examples/many-lights-demo.js"
@@ -17,6 +7,21 @@ import {Scene_To_Texture_Demo} from "./examples/scene-to-texture-demo.js"
 import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
 import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
+import {Game} from "./Game.js";
+
+// Pull these names into this module's scope for convenience:
+const {
+    Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
+    Canvas_Widget, Code_Widget, Text_Widget
+} = tiny;
+
+// Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
+// This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
+
+// ******************** Extra step only for when executing on a local machine:
+//                      Load any more files in your directory and copy them into "defs."
+//                      (On the web, a server should instead just pack all these as well
+//                      as common.js into one file for you, such as "dependencies.js")
 
 const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
 
@@ -28,84 +33,15 @@ Object.assign(defs,
     {Scene_To_Texture_Demo},
     {Surfaces_Demo},
     {Text_Demo},
-    {Transforms_Sandbox}
+    {Transforms_Sandbox},
+    {Game}
 );
-*/
-//export {Canvas_Widget, Code_Widget, Text_Widget, defs}
 
-export class Main_Scene extends Scene {
-    constructor() {
-        super();
-        this.maze = new Maze();
-        this.player = new Player();
-        this.player.position = this.maze.start_position;
-        this.initial_camera_location = Mat4.look_at(vec3(15, 50, 40), vec3(15, 0, 20), vec3(0, 1, 0));
-        //NOTE: Current alternative solution to the control panel.
-        window.addEventListener('keydown', (event) => {
-            switch (event.key) {
-                case 'ArrowLeft':
-                    this.player.turn_left();
-                    break;
-                case 'ArrowRight':
-                    this.player.turn_right();
-                    break;
-                case 'ArrowUp':
-                    this.player.move_forward();
-                    if (this.maze.checkEnd(this.player.position)) {
-                        console.log('the end');
-                        window.onReachEnd();
-                    }
-                    // if (checkEnd(this.player.position, this.maze)) window.onReachEnd(); 
-                    break;
-                case 'ArrowDown':
-                    this.player.move_backward();
-                    if (this.maze.checkEnd(this.player.position)) {
-                        console.log('the end');
-                        window.onReachEnd();
-                    }
-                    // if (checkEnd(this.player.position, this.maze)) window.onReachEnd();                  
-                    break;
-                case 'Delete':
-                    window.onReachEnd();
-            }
-        });
-    }
+// ******************** End extra step
 
-    //NOTE: Not sure, why this method doesn't work, but the above one does. Should probably ask a TA.
-    /*
-    make_control_panel() {
-        this.key_triggered_button("Move Player Left", ['ArrowLeft'], () => {
-            console.log("Turning Left!")
-            this.player.turn_left()
-        });
-        this.new_line();
-        this.key_triggered_button("Move Player Right", ['ArrowRight'], () => this.player.turn_right());
-        this.new_line();
-        this.key_triggered_button("Move Player Forward", ['ArrowUp'], () => this.player.move_forward(this.maze));
-        this.new_line()
-        this.key_triggered_button("Move Player Back", ['ArrowDown'], () => this.player.move_backward(this.maze));
-    }
-    */
+// (Can define Main_Scene's class here)
 
-    
+const Main_Scene = Game;
+const Additional_Scenes = [];
 
-    display(context, program_state) {
-        //Setting the animation time
-        let t = program_state.animation_time / 1000.0; // ms -> s
-
-        // Set the initial camera location
-        program_state.set_camera(this.initial_camera_location);
-        program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 0.1, 1000);
-
-        // Set up the light
-        const light_position = vec4(20, 50, 20, 1);
-        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
-
-        // Display the maze
-        this.maze.display(context, program_state);
-        
-        // Display the player
-        this.player.display(context, program_state);
-    }
-}
-
+export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
